54a83e4344495bf9b40de2559ea8a8c9e52f8025
diksed <diksed00@gmail.com>
2023-06-24 16:09:28 +0300

    Added SplashScreen

---
+++ app/src/main/java/com/sedatkavak/kriptak/screens/splash_screen/SplashActivity.kt
@@ -0,0 +1,30 @@
+package com.sedatkavak.kriptak.screens.splash_screen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
+import com.sedatkavak.kriptak.MainActivity
+import com.sedatkavak.kriptak.R
+import com.sedatkavak.kriptak.databinding.ActivitySplashBinding
+
+class SplashActivity : AppCompatActivity() {
+    private lateinit var binding : ActivitySplashBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val frontLogo: ImageView = binding.logoImageView
+        val imgStartAnim = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
+        frontLogo.startAnimation(imgStartAnim)
+        Handler().postDelayed(Runnable {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 1600)
+    }
+}
