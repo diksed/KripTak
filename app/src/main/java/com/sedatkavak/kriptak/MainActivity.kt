2f607c0d42221064d00f6311999fe18c25c4a976
diksed <diksed00@gmail.com>
2023-06-25 21:15:46 +0300

    Added BottomNavigationBar

--- app/src/main/java/com/sedatkavak/kriptak/MainActivity.kt
+++ app/src/main/java/com/sedatkavak/kriptak/MainActivity.kt
@@ -5,11 +5,10 @@ import android.os.Bundle
 import com.sedatkavak.kriptak.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding : ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
     }
 }
