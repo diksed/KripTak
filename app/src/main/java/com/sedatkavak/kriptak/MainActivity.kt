89849feee92bc46e6ae0cca926dc7179958075c0
diksed <diksed00@gmail.com>
2023-06-25 18:03:45 +0300

    Added Homepage basically

--- app/src/main/java/com/sedatkavak/kriptak/MainActivity.kt
+++ app/src/main/java/com/sedatkavak/kriptak/MainActivity.kt
@@ -2,10 +2,14 @@ package com.sedatkavak.kriptak
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.sedatkavak.kriptak.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
--- app/src/main/java/com/sedatkavak/kriptak/screens/splash_screen/SplashActivity.kt
+++ app/src/main/java/com/sedatkavak/kriptak/screens/splash_screen/SplashActivity.kt
@@ -25,6 +25,6 @@ class SplashActivity : AppCompatActivity() {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 1600)
+        }, 1700)
     }
 }
